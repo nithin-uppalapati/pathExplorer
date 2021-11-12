c17ae654a8c8b1af16370aa8415559bca4ab99d7
Nithin Uppalapati <ee18b035@smail.iitm.ac.in>
2021-11-12 18:39:42 +0530

    updated path process file

--- Desktop_APP/path_processing.py
+++ Desktop_APP/path_processing.py
@@ -5,13 +5,15 @@ def euc_dist(a,b):
     res = np.sqrt(res)
     return res
 
-def ang_calc(a,b):          # have to use complex numbers to do the sign angle thing | vectors won't work
-    res = np.dot(a,b)
-    A = np.linalg.norm(a)
-    B = np.linalg.norm(b)
-    res = res / (A*B)
+def ang_calc(a,b,c):          # have to use complex numbers to do the sign angle thing | vectors won't work
+    Y1 = b[1] - a[1]
+    X1 = b[0] - a[0]
+    Y2 = - b[1] + c[1]
+    X2 = - b[0] + c[0]
+    ang1 = np.degrees(np.arctan2(Y1,X1))
+    ang2 = np.degrees(np.arctan2(Y2,X2))
+    res = ang2 - ang1
     return res
-
 
 
 def dist_ang_calctor(data_points, offset_angle):            # n-points has n-1 edges and n-2 turns
